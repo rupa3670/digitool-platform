26caf3c64366f9d63204efc306fb0ee9cbec5634
Tanjila Rupa <srrupaliakter@gmail.com>
2026-04-07 11:03:23 +0600

    create digitool-platform

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h2 className='text-2xl text-red-500 bg-black border-2'>Hello world!</h2>
+    </>
+  )
+}
+
+export default App
