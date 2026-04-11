422c1ba4f44440f999dde7af1d9698e8a4931df3
Tanjila Rupa <srrupaliakter@gmail.com>
2026-04-11 21:48:10 +0600

    fix cart logo

--- src/compoents/homepage/Tabs.jsx
+++ src/compoents/homepage/Tabs.jsx
@@ -6,7 +6,7 @@ const Tabs = ({activeTab,setActiveTab,carts=[]}) => {
    <div className='flex justify-center items-center bg-gray-50 p-2 rounded-full w-fit mx-auto mb-10 gap-3'>
     <button onClick={()=>setActiveTab("Products")} className={` px-8 py-3  rounded-full font-bold transition-all ${activeTab==="Products" ? "bg-linear-to-r from-blue-900 to-purple-500 text-white " :"text-gray-500 hover:bg-gray-200"}`}>Products</button>
     <button onClick={()=>setActiveTab("Cart")} className={` px-8 py-3  rounded-full font-bold transition-all ${activeTab==="Cart" ? "bg-linear-to-r from-blue-900 to-purple-500 text-white " :"text-gray-500 hover:bg-gray-200"}`}>
-      Car({carts.length})  
+      Cart({carts.length})  
     </button>
 
    </div>
